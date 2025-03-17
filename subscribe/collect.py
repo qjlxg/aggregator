b5ffa843c3e566a10fd491248b571fa895d08367
qjlxg <qjlxg@163.com>
2025-03-17 22:41:06 +0800

    Update collect.py

--- subscribe/collect.py
+++ subscribe/collect.py
@@ -140,7 +140,7 @@ def assign(
     # 爬取新站点列表
     if not domains or overwrite:
         candidates = crawl.collect_airport(
-            channel="jichang_list",
+            channel="freeVPNjd",
             page_num=pages,
             num_thread=num_threads,
             rigid=rigid,
