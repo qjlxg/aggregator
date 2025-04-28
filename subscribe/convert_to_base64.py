5db9edc86d772c4692f40aabff2454ccccb873ec
qjlxg <qjlxg@163.com>
2025-04-28 11:55:00 +0800

    Update convert_to_base64.py

--- subscribe/convert_to_base64.py
+++ subscribe/convert_to_base64.py
@@ -37,13 +37,13 @@ def convert_multiple_to_base64(urls):
 if __name__ == "__main__":
     urls = [
        # "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/data/ss.txt",
-       # "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/data/transporter.txt",
+        "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/data/transporter.txt",
         "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/data/clash.yaml",
         "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/base.txt",
         "https://raw.githubusercontent.com/qjlxg/cheemsar/refs/heads/main/Long_term_subscription_num",
         "https://raw.githubusercontent.com/qjlxg/cheemsar-2/refs/heads/main/Long_term_subscription_num",
-      #  "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/Long_term_subscription_num",
-      #  "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/all_clash.txt",
+        "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/Long_term_subscription_num",
+        "https://raw.githubusercontent.com/qjlxg/license/refs/heads/main/all_clash.txt",
         
     ]
     convert_multiple_to_base64(urls)
