52a71173eaa02c72c54c09784a2301d6e60db98d
qjlxg <qjlxg@163.com>
2025-05-05 01:20:12 +0800

    Update replace.py

--- subscribe/replace.py
+++ subscribe/replace.py
@@ -289,7 +289,7 @@ def main(urls):
 if __name__ == "__main__":
     urls = [
         'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/clash.yaml',
-        'https://raw.githubusercontent.com/qjlxg/hy2/refs/heads/main/configtg.txt',
+       # 'https://raw.githubusercontent.com/qjlxg/hy2/refs/heads/main/configtg.txt',
         
         'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/Long_term_subscription_num',
         'https://raw.githubusercontent.com/qjlxg/cheemsar/refs/heads/main/Long_term_subscription_num',
