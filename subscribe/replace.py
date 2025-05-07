c505d89f7140114871536c23b98fbbdc61ab5060
qjlxg <qjlxg@163.com>
2025-05-07 23:04:52 +0800

    Update replace.py

--- subscribe/replace.py
+++ subscribe/replace.py
@@ -288,12 +288,13 @@ def main(urls):
 
 if __name__ == "__main__":
     urls = [
-        'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/ss.txt',
-        'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/all_clash.txt',
-        'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/base64.txt',
-        'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/Long_term_subscription_num',
-        'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/clash.yaml',
-        'https://raw.githubusercontent.com/qjlxg/cheemsar/refs/heads/main/Long_term_subscription_num',
-        'https://raw.githubusercontent.com/qjlxg/cheemsar-2/refs/heads/main/Long_term_subscription_num',
+       # 'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/ss.txt',
+       # 'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/all_clash.txt',
+       # 'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/base64.txt',
+       # 'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/Long_term_subscription_num',
+      #  'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/clash.yaml',
+       # 'https://raw.githubusercontent.com/qjlxg/cheemsar/refs/heads/main/Long_term_subscription_num',
+       # 'https://raw.githubusercontent.com/qjlxg/cheemsar-2/refs/heads/main/Long_term_subscription_num',
+        'https://github.com/qjlxg/collectSub/raw/refs/heads/main/all_nodes.txt',
     ]
     main(urls)
