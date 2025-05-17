346ff31885dd976e14064915bb5614191c7aa4c5
qjlxg <qjlxg@163.com>
2025-05-17 08:32:59 +0800

    Update convert_to_base64.py

--- subscribe/convert_to_base64.py
+++ subscribe/convert_to_base64.py
@@ -37,7 +37,6 @@ def convert_multiple_to_base64(urls):
 if __name__ == "__main__":
     urls = [
         "https://github.com/qjlxg/aggregator/raw/refs/heads/main/base.txt",
-        "",
         "https://kurzlinks.de/13tc",
     
         
