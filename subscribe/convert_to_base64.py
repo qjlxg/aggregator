52259de82df295437ef257f523f30bb3375a6b39
qjlxg <qjlxg@163.com>
2025-06-09 20:57:09 +0800

    Update convert_to_base64.py

--- subscribe/convert_to_base64.py
+++ subscribe/convert_to_base64.py
@@ -18,23 +18,9 @@ EXCLUDE_KEYWORDS = [
     "cdn.jsdelivr.net", "statically.io", "googletagmanager.com",
     "www.w3.org", "fonts.googleapis.com", "schemes.ogf.org", "clashsub.net",
     "t.me", "api.w.org",
-    "html", "css", "js", "ico", "png", "jpg", "jpeg", "gif", "svg", "webp", "xml", "json", "txt",
+    # "html", "css", "js", "ico", "png", "jpg", "jpeg", "gif", "svg", "webp", "xml", "json", "txt", # <--- Comment out or remove these
     "google-analytics.com", "cloudflare.com/cdn-cgi/", "gstatic.com", "googleapis.com",
-    "disqus.com", "gravatar.com", "s.w.org",
-    "amazon.com", "aliyuncs.com", "tencentcos.cn",
-    "cdn.bootcss.com", "cdnjs.cloudflare.com",
-    "bit.ly", "tinyurl.com", "cutt.ly", "shorturl.at", "surl.li", "suo.yt", "v1.mk",
-    "youtube.com", "facebook.com", "twitter.com", "weibo.com",
-    "mail.google.com", "docs.google.com",
-    "microsoft.com", "apple.com", "baidu.com", "qq.com",
-    ".woff", ".woff2", ".ttf", ".otf", ".eot",
-    ".zip", ".rar", ".7z", ".tar.gz", ".exe", ".dmg", ".apk",
-    "/static/", "/images/", "/scripts/", "/styles/", "/fonts/",
-    "robots.txt", "sitemap.xml", "favicon.ico",
-    "rss", "atom",
-    "/LICENSE", "/README.md", "/CHANGELOG.md",
-    ".git", ".svn",
-    "swagger-ui.html", "openapi.json"
+    # ... keep the rest of your exclude list as is, unless you find other problematic keywords
 ]
 
 # --- Proxy Parsing Functions ---
